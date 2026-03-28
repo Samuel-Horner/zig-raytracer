14cae273831ca4c79ec88752bf87412e060a4619
Samuel Horner <samkoseyhorner@gmail.com>
2026-03-28 16:20:40 +0000

    Added compute shader pipeline

--- src/engine.zig
+++ src/engine.zig
@@ -1,7 +1,7 @@
+const std = @import("std");
+
 const gl = @import("gl");
 const glfw = @import("glfw");
-
-const std = @import("std");
 
 const debug = @import("debug.zig");
 
@@ -20,22 +20,350 @@ pub const Window = struct {
         };
     }
 
-    fn makeCurrent(self: *Window) void {
+    fn makeCurrent(self: *const Window) void {
         glfw.makeContextCurrent(self.id);
     }
 
     fn deinit(self: *Window) void {
         glfw.destroyWindow(self.id);
     }
+
+    pub fn shouldClose(self: *const Window) bool {
+        return glfw.windowShouldClose(self.id);
+    }
+
+    pub fn keyPressed(self: *const Window, key: c_int) bool {
+        return glfw.getKey(self.id, key) == glfw.Press;
+    }
+
+    pub fn close(self: *const Window) void {
+        glfw.setWindowShouldClose(self.id, true);
+    }
+};
+
+// Allows for uniform function declaration with struct ownership.
+pub const UniformFunction = union {
+    basic: *const fn (c_int) void,
+    owned: *const fn (*anyopaque, c_int) void,
+};
+
+pub const Uniform = struct {
+    location: c_int,
+    function: UniformFunction,
+
+    fn applyOwned(self: *const Uniform, owner: *anyopaque) void {
+        self.function.owned(owner, self.location);
+    }
+
+    fn apply(self: *const Uniform) void {
+        self.function.basic(self.location);
+    }
+
+    fn init(location: c_int, function: UniformFunction) Uniform {
+        return Uniform{ .location = location, .function = function };
+    }
+};
+
+fn compileShader(shader: c_uint, source: []const u8) !void {
+    gl.ShaderSource(shader, 1, &.{source.ptr}, &.{@intCast(source.len)});
+    gl.CompileShader(shader);
+
+    var success: i32 = undefined;
+    gl.GetShaderiv(shader, gl.COMPILE_STATUS, (&success)[0..1]);
+    if (success != 1) {
+        var info_log: [512:0]u8 = undefined;
+        gl.GetShaderInfoLog(shader, info_log.len, null, &info_log);
+        debug.err("Shader {} failed to compile.\n{s}", .{ shader, std.mem.sliceTo(&info_log, 0) });
+
+        return error.ShaderCompilationFailed;
+    }
+}
+
+pub const Texture = struct {
+    const TextureOpts = struct {
+        internal_format: u32 = gl.RGBA32F,
+        format: u32 = gl.RGBA,
+        type: u32 = gl.FLOAT,
+    };
+
+    id: c_uint,
+    unit: u32,
+
+    width: c_int,
+    height: c_int,
+
+    opts: TextureOpts,
+
+    pub fn init(comptime unit: comptime_int, width: c_int, height: c_int, opts: TextureOpts) Texture {
+        var texture: Texture = undefined;
+
+        texture.unit = unit;
+
+        texture.width = width;
+        texture.height = height;
+
+        texture.opts = opts;
+
+        gl.GenTextures(1, (&texture.id)[0..1]);
+        texture.activate();
+        gl.BindTexture(gl.TEXTURE_2D, texture.id);
+
+        // Basic Texture Parameters
+        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+        gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+        return texture;
+    }
+
+    pub fn empty(self: *Texture, level: c_int) void {
+        gl.TexImage2D(gl.TEXTURE_2D, level, @intCast(self.opts.internal_format), self.width, self.height, 0, self.opts.format, self.opts.type, null);
+    }
+
+    pub fn bind(self: *Texture, level: c_int, comptime mode: comptime_int) void {
+        gl.BindImageTexture(self.unit - gl.TEXTURE0, self.id, level, gl.FALSE, 0, mode, self.opts.internal_format);
+    }
+
+    pub fn activate(self: *Texture) void {
+        gl.ActiveTexture(self.unit);
+    }
+
+    pub fn apply(self: *anyopaque, location: c_int) void {
+        const self_ptr: *Texture = @ptrCast(@alignCast(self));
+        gl.Uniform1i(location, @intCast(self_ptr.unit - gl.TEXTURE0));
+    }
+
+    pub fn resize(self: *Texture, level: c_int, width: c_int, height: c_int) void {
+        self.width = width;
+        self.height = height;
+
+        self.empty(level);
+    }
+};
+
+pub const ComputeProgram = struct {
+    id: c_uint,
+    uniforms: std.ArrayList(Uniform),
+
+    fn linkProgram(self: *ComputeProgram, compute_shader: c_uint) !void {
+        gl.AttachShader(self.id, compute_shader);
+
+        gl.LinkProgram(self.id);
+
+        // Check program link status
+        var success: i32 = undefined;
+        gl.GetProgramiv(self.id, gl.LINK_STATUS, (&success)[0..1]);
+        if (success != 1) {
+            var info_log: [512:0]u8 = undefined;
+            gl.GetProgramInfoLog(self.id, info_log.len, null, &info_log);
+            debug.err("Program {} failed to link.\n{s}", .{ self.id, std.mem.sliceTo(&info_log, 0) });
+
+            return error.ProgramLinkFailed;
+        }
+    }
+
+    pub fn init(compute_source: []const u8) !ComputeProgram {
+        var compute_program: ComputeProgram = undefined;
+
+        const compute_shader: c_uint = gl.CreateShader(gl.COMPUTE_SHADER);
+        try compileShader(compute_shader, compute_source);
+
+        compute_program.id = gl.CreateProgram();
+        try compute_program.linkProgram(compute_shader);
+
+        debug.log("Created compute program {}.", .{compute_program.id});
+
+        gl.DeleteShader(compute_shader);
+
+        compute_program.uniforms = .empty;
+
+        return compute_program;
+    }
+
+    pub fn deinit(self: *ComputeProgram) void {
+        self.uniforms.deinit(engine_allocator);
+    }
+
+    pub fn use(self: *ComputeProgram) void {
+        gl.UseProgram(self.id);
+    }
+
+    pub fn dispatch(self: *ComputeProgram, x: u32, y: u32, z: u32) void {
+        gl.UseProgram(self.id);
+
+        gl.DispatchCompute(x, y, z);
+
+        gl.MemoryBarrier(gl.SHADER_IMAGE_ACCESS_BARRIER_BIT);
+    }
+
+    pub fn registerUniform(self: *ComputeProgram, comptime name: [:0]const u8, function: UniformFunction) !usize {
+        const location = gl.GetUniformLocation(self.id, name.ptr);
+
+        if (location == -1) {
+            debug.err("Failed to find uniform \"{s}\" in program {}.", .{ name, self.id });
+            return error.UniformNotFound;
+        }
+
+        try self.uniforms.append(engine_allocator, Uniform.init(location, function));
+
+        return self.uniforms.items.len - 1;
+    }
+
+    pub fn applyAllUniforms(self: *ComputeProgram) void {
+        for (self.uniforms.items) |uniform| {
+            uniform.apply();
+        }
+    }
+
+    pub fn applyUniform(self: *ComputeProgram, uniform: usize) void {
+        self.uniforms.items[uniform].apply();
+    }
+
+    pub fn applyOwnedUniform(self: *ComputeProgram, uniform: usize, owner: *anyopaque) void {
+        self.uniforms.items[uniform].applyOwned(owner);
+    }
+};
+
+// Simple Vertex + Fragment Program
+pub const Program = struct {
+    id: c_uint,
+    uniforms: std.ArrayList(Uniform),
+
+    fn linkProgram(self: *Program, vertex_shader: c_uint, fragment_shader: c_uint) !void {
+        gl.AttachShader(self.id, vertex_shader);
+        gl.AttachShader(self.id, fragment_shader);
+
+        gl.LinkProgram(self.id);
+
+        // Check program link status
+        var success: i32 = undefined;
+        gl.GetProgramiv(self.id, gl.LINK_STATUS, (&success)[0..1]);
+        if (success != 1) {
+            var info_log: [512:0]u8 = undefined;
+            gl.GetProgramInfoLog(self.id, info_log.len, null, &info_log);
+            debug.err("Program {} failed to link.\n{s}", .{ self.id, std.mem.sliceTo(&info_log, 0) });
+
+            return error.ProgramLinkFailed;
+        }
+    }
+
+    pub fn init(vertex_source: []const u8, fragment_source: []const u8) !Program {
+        var program: Program = undefined;
+
+        // Vertex Shader
+        const vertex_shader: c_uint = gl.CreateShader(gl.VERTEX_SHADER);
+        try compileShader(vertex_shader, vertex_source);
+
+        // Fragment Shader
+        const fragment_shader: c_uint = gl.CreateShader(gl.FRAGMENT_SHADER);
+        try compileShader(fragment_shader, fragment_source);
+
+        // Create and link program
+        program.id = gl.CreateProgram();
+        try program.linkProgram(vertex_shader, fragment_shader);
+
+        debug.log("Created program {}.", .{program.id});
+
+        // Delete shaders
+        gl.DeleteShader(vertex_shader);
+        gl.DeleteShader(fragment_shader);
+
+        // Setup Uniform List
+        program.uniforms = .empty;
+
+        return program;
+    }
+
+    pub fn registerUniform(self: *Program, comptime name: [:0]const u8, function: UniformFunction) !usize {
+        const location = gl.GetUniformLocation(self.id, name.ptr);
+
+        if (location == -1) {
+            debug.err("Failed to find uniform \"{s}\" in program {}.", .{ name, self.id });
+            return error.UniformNotFound;
+        }
+
+        try self.uniforms.append(engine_allocator, Uniform.init(location, function));
+
+        return self.uniforms.items.len - 1;
+    }
+
+    pub fn use(self: *Program) void {
+        gl.UseProgram(self.id);
+    }
+
+    pub fn applyAllUniforms(self: *Program) void {
+        for (self.uniforms.items) |uniform| {
+            uniform.apply();
+        }
+    }
+
+    pub fn applyUniform(self: *Program, uniform: usize) void {
+        self.uniforms.items[uniform].apply();
+    }
+
+    pub fn applyOwnedUniform(self: *Program, uniform: usize, owner: *anyopaque) void {
+        self.uniforms.items[uniform].applyOwned(owner);
+    }
+
+    pub fn deinit(self: *Program) void {
+        self.uniforms.deinit(engine_allocator);
+    }
+};
+
+pub const QuadRenderer = struct {
+    const verts: [24]f32 = .{
+        -1, 1,  0, 0,
+        -1, -1, 0, 1,
+        1,  -1, 1, 1,
+        -1, 1,  0, 0,
+        1,  -1, 1, 1,
+        1,  1,  1, 0,
+    };
+
+    program: Program,
+
+    vao: c_uint,
+    vbo: c_uint,
+
+    pub fn init(vertex_source: []const u8, fragment_source: []const u8) !QuadRenderer {
+        var quad_renderer: QuadRenderer = undefined;
+
+        quad_renderer.program = try Program.init(vertex_source, fragment_source);
+
+        gl.GenVertexArrays(1, (&quad_renderer.vao)[0..1]);
+        gl.GenBuffers(1, (&quad_renderer.vbo)[0..1]);
+
+        gl.BindVertexArray(quad_renderer.vao);
+        gl.BindBuffer(gl.ARRAY_BUFFER, quad_renderer.vao);
+
+        gl.BufferData(gl.ARRAY_BUFFER, @intCast(@sizeOf(f32) * QuadRenderer.verts.len), &QuadRenderer.verts, gl.STATIC_DRAW);
+
+        gl.VertexAttribPointer(0, 4, gl.FLOAT, gl.FALSE, 4 * @sizeOf(f32), 0);
+        gl.EnableVertexAttribArray(0);
+
+        return quad_renderer;
+    }
+
+    pub fn deinit(self: *QuadRenderer) void {
+        self.program.deinit();
+    }
+
+    pub fn render(self: *QuadRenderer) void {
+        self.program.use();
+
+        gl.BindVertexArray(self.vao);
+        gl.DrawArrays(gl.TRIANGLES, 0, 6);
+    }
 };
 
 var engine_allocator: std.mem.Allocator = undefined;
 
-const SizeCallback = *const fn(c_int, c_int) void;
+const SizeCallback = *const fn (c_int, c_int) void;
 var size_callbacks: std.ArrayList(SizeCallback) = .empty;
 
 pub fn registerSizeCallback(callback: SizeCallback) !void {
-    size_callbacks.append(engine_allocator, callback);
+    try size_callbacks.append(engine_allocator, callback);
     debug.log("Registered new frame buffer size callback.", .{});
 }
 
@@ -61,6 +389,10 @@ pub fn init(allocator: std.mem.Allocator, window_width: c_int, window_height: c_
     try glfw.init();
     debug.log("Initialised GLFW {s}", .{glfw.getVersionString()});
 
+    // Window Hints
+    glfw.windowHint(glfw.ContextVersionMajor, 4);
+    glfw.windowHint(glfw.ContextVersionMinor, 6);
+
     // Create Window
     window = try Window.init(window_width, window_height, name);
     window.makeCurrent();
@@ -70,9 +402,10 @@ pub fn init(allocator: std.mem.Allocator, window_width: c_int, window_height: c_
     // Bind GL Procs
     if (!procs.init(glfw.getProcAddress)) return error.InitError;
     gl.makeProcTableCurrent(&procs);
+    debug.log("Loaded OpenGL {s} {}.{}.", .{ @tagName(gl.info.profile orelse "unkown"), gl.info.version_major, gl.info.version_minor });
 
     // Issue GL Configurations
-    gl.ClearColor(1, 1, 1, 1);
+    gl.ClearColor(1, 0, 1, 1);
 }
 
 pub fn deinit() void {
@@ -81,18 +414,16 @@ pub fn deinit() void {
     glfw.makeContextCurrent(null);
     window.deinit();
 
+    size_callbacks.deinit(engine_allocator);
+
     glfw.terminate();
 }
 
-pub fn render() void {
-    while (!glfw.windowShouldClose(window.id)) {
-        if (glfw.getKey(window.id, glfw.KeyEscape) == glfw.Press) {
-            glfw.setWindowShouldClose(window.id, true);
-        }
+pub fn clearViewport() void {
+    gl.Clear(gl.COLOR_BUFFER_BIT);
+}
 
-        gl.Clear(gl.COLOR_BUFFER_BIT);
-
-        glfw.pollEvents();
-        glfw.swapBuffers(window.id);
-    }
+pub fn finishRender() void {
+    glfw.pollEvents();
+    glfw.swapBuffers(window.id);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,16 @@
 const std = @import("std");
 
+const gl = @import("gl");
+const glfw = @import("glfw");
+
 const engine = @import("engine.zig");
 const debug = @import("debug.zig");
+
+var output_texture: engine.Texture = undefined;
+
+fn resizeOutputTexture(width: c_int, height: c_int) void {
+    output_texture.resize(0, width, height);
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}).init;
@@ -12,5 +21,35 @@ pub fn main() !void {
     try engine.init(gpa.allocator(), 800, 460, "Hello World");
     defer engine.deinit();
 
-    engine.render();
+    var quad_renderer = try engine.QuadRenderer.init(@embedFile("shader/quad_vert.glsl"), @embedFile("shader/quad_frag.glsl"));
+    defer quad_renderer.deinit();
+
+    var compute_program = try engine.ComputeProgram.init(@embedFile("shader/compute.glsl"));
+    defer compute_program.deinit();
+
+    const out_unit = gl.TEXTURE0;
+
+    output_texture = engine.Texture.init(out_unit, engine.window.width, engine.window.height, .{});
+    output_texture.empty(0);
+    output_texture.bind(0, gl.READ_WRITE);
+
+    try engine.registerSizeCallback(resizeOutputTexture);
+
+    quad_renderer.program.use();
+    const output_texture_uniform = try quad_renderer.program.registerUniform("tex", .{ .owned = engine.Texture.apply });
+    quad_renderer.program.applyOwnedUniform(output_texture_uniform, &output_texture);
+
+    while (!engine.window.shouldClose()) {
+        if (engine.window.keyPressed(glfw.KeyEscape)) {
+            engine.window.close();
+        }
+
+        engine.clearViewport();
+
+        compute_program.dispatch(@intCast(output_texture.width), @intCast(output_texture.height), 1);
+
+        quad_renderer.render();
+
+        engine.finishRender();
+    }
 }
